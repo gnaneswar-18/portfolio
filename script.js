436500c65c74581793557cccf02bfd09dd16fde0
gnaneswar-18 <satyagnaneswarchilakala@gmail.com>
2025-12-31 19:22:43 +0530

    updated portfolio

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
-function shownav() {
-    var navbar = document.querySelector('.navbar');
-    var btn=document.getElementById('bar-icon');
-    if (navbar.style.visibility === 'hidden') {
-        navbar.style.visibility = 'visible';
-        navbar.style.zIndex = '1000';
-        btn.classList='';
-        btn.classList='fa-solid fa-x';
-    } else {
-        navbar.style.visibility = 'hidden';
-        navbar.style.zIndex = '-1'; 
-        btn.classList='';
-        btn.classList='fa-solid fa-bars';
-    }
-}
-function scrolltotop(){
-    window.scrollTo(0,0)
-}
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+};
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
+};
